e0e0254ebf2630497f48ca9274e721bd5b7af5c5
Cedric Pradalier <cedric.pradalier@mavt.ethz.ch>
2012-10-01 12:47:29 +0200

    Cleanup

---
+++ task_manager_lib/include/task_manager_lib/TaskIdleDefault.h
@@ -0,0 +1,27 @@
+#ifndef TASK_IDLE_DEFAULT_H
+#define TASK_IDLE_DEFAULT_H
+
+#include "task_manager_lib/MinimalTaskConfig.h"
+#include "task_manager_lib/TaskDefinition.h"
+
+namespace task_manager_lib {
+    class TaskIdleDefault : public TaskDefinitionWithConfig<task_manager_lib::MinimalTaskConfig>
+    {
+        protected:
+        public:
+            TaskIdleDefault(TaskEnvironment *env) 
+                : TaskDefinitionWithConfig<task_manager_lib::MinimalTaskConfig>("Idle","Do nothing",true,-1) {}
+            virtual ~TaskIdleDefault() {};
+
+            virtual TaskIndicator configure(const TaskParameters & parameters) throw (InvalidParameter);
+
+            virtual TaskIndicator initialise(const TaskParameters & parameters) throw (InvalidParameter);
+
+            virtual TaskIndicator iterate();
+
+            virtual TaskIndicator terminate();
+
+    };
+};
+
+#endif // TASK_IDLE_DEFAULT_H
