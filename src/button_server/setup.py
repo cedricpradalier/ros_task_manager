b302b61735a3f69141709ce6854423b373ab0c85
Cedric Pradalier <cedric.pradalier@gmail.com>
2014-01-28 22:19:46 +0100

    Removed debug print

--- src/button_server/setup.py
+++ src/button_server/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-print("ButtonServer: Setup.py")
+# print("ButtonServer: Setup.py")
 
 d = generate_distutils_setup(
    ##  don't do this unless you want a globally visible script
