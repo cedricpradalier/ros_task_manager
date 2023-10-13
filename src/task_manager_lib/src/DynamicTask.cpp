cd859f79d0bf482e827163cde871fb0569e0cb03
Cedric Pradalier <cedric.pradalier@gmail.com>
2023-10-13 23:37:17 +0200

    Fixed compatibility with --merge-install

--- src/task_manager_lib/src/DynamicTask.cpp
+++ src/task_manager_lib/src/DynamicTask.cpp
@@ -5,8 +5,12 @@ using namespace task_manager_lib;
 
 
 DynamicTask::DynamicTask(const std::string & fname, TaskEnvironmentPtr env) :
-	TaskDefinitionBase("DynamicTask","undefined",true,env,TaskConfigPtr()),filename(fname) 
-{
+	TaskDefinitionBase("DynamicTask","undefined",true,env,TaskConfigPtr()),filename(fname) {
+        task.reset();
+        handle = NULL;
+    }
+
+bool DynamicTask::loadTask(bool exceptions) {
 	// Hack to garantee alignment on 32 bits, for valgrind
 	char fbuffer[1024];
 	strncpy(fbuffer,filename.c_str(),1023);
@@ -17,29 +21,49 @@ DynamicTask::DynamicTask(const std::string & fname, TaskEnvironmentPtr env) :
         RCLCPP_ERROR(node->get_logger(),"dlopen: %s",dlerror());
 		std::string error("dlopen returned NULL handle for file '");
 		error += filename + "'";
-		throw DLLoadError(error);
+        if (exceptions) {
+            throw DLLoadError(error);
+        } else {
+            RCLCPP_ERROR(node->get_logger(),"%s",DLLoadError(error).what());
+            return false;
+        }
 	}
 	TaskFactory tf = (TaskFactory)dlsym(handle, "TaskFactoryObject");
     if (!tf) {
-		throw DLLoadError("The TaskFactoryObject is NULL. The task probably missed the DYNAMIC_TASK macro");
+		dlclose(handle);
+        handle = NULL;
+        if (exceptions) {
+            throw DLLoadError("The TaskFactoryObject is NULL. The task probably missed the DYNAMIC_TASK macro");
+        } else {
+            RCLCPP_ERROR(node->get_logger(),"%s",DLLoadError("The TaskFactoryObject is NULL. The task probably missed the DYNAMIC_TASK macro").what());
+            return false;
+        }
     }
-	task = tf(env);
+	task = tf(env_gen);
 	if (!task) {
 		dlclose(handle);
 		handle = NULL;
-		throw DLLoadError("taskfactory returned NULL task");
+        if (exceptions) {
+            throw DLLoadError("taskfactory returned NULL task");
+        } else {
+            RCLCPP_ERROR(node->get_logger(),"%s",DLLoadError("taskfactory returned NULL task").what());
+            return false;
+        }
 	}
     // Do not declare parameters. It has been done in the loaded task. The
     // dynamic task has no parameter of its own.
     // cfg_gen = task->cfg_gen;
+    return true;
 }
 
 DynamicTask::~DynamicTask()
 {
 	// printf("Deleting dynamic task: ");
 	task.reset();
-	dlclose(handle);
-	handle = NULL;
+    if (handle) {
+        dlclose(handle);
+        handle = NULL;
+    }
 }
 
 
