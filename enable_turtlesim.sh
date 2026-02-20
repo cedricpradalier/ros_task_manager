8f893943d8d0955a4a1d85826e92cd29a53e1e49
Cedric Pradalier <cedric.pradalier@gmail.com>
2026-02-20 15:58:22 +0100

    Updated scripts to manage example packages

---
+++ enable_turtlesim.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd src
+rm -f task_manager_turtlesim/COLCON_IGNORE
+rm -f task_manager_turtlesim_sync/COLCON_IGNORE
+rm -f task_manager_turtlesim_smach/COLCON_IGNORE
+rm -f turtlesim_logo/COLCON_IGNORE
+
--- ignore_turtlesim.sh
+++ ignore_turtlesim.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 cd src
-touch task_manager_turtlesim/CATKIN_IGNORE
-touch task_manager_turtlesim_sync/CATKIN_IGNORE
-touch task_manager_turtlesim_smach/CATKIN_IGNORE
-touch turtlesim_logo/CATKIN_IGNORE
-git add task_manager_turtlesim/CATKIN_IGNORE
-git add task_manager_turtlesim_sync/CATKIN_IGNORE
-git add task_manager_turtlesim_smach/CATKIN_IGNORE
-git add turtlesim_logo/CATKIN_IGNORE
+touch task_manager_turtlesim/COLCON_IGNORE
+touch task_manager_turtlesim_sync/COLCON_IGNORE
+touch task_manager_turtlesim_smach/COLCON_IGNORE
+touch turtlesim_logo/COLCON_IGNORE
 
