cbbf17d700d614954a2069160006fe4315f3afce
Cedric Pradalier <cedric.pradalier@gmail.com>
2026-07-03 13:44:30 +0200

    Fixed nav2 linking

--- ignore_turtlesim.sh
+++ ignore_turtlesim.sh
@@ -3,6 +3,4 @@
 cd src
 touch task_manager_turtlesim/COLCON_IGNORE
 touch task_manager_turtlesim_sync/COLCON_IGNORE
-touch task_manager_turtlesim_smach/COLCON_IGNORE
-touch turtlesim_logo/COLCON_IGNORE
 
